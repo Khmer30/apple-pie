dad6b57108b9b7fc40645a7918757ea8f9f1978b
marie <dul.sreyneang@gmail.com>
2022-02-14 11:16:08 +0800

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Joy Marie on 2/12/22.
+//
+
+import UIKit
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+//    func updateUI() {
+//        var letters = [String] ()
+//        for letter in currentGame.formattedWord {
+//            letters.append(String(letter))
+//        }
+//    }
+}
+
+var listOfWords = ["buccaneer", "swift", "glorious", "incandesent", "bug", "program"]
+
+let incorrectMovesAllowed = 7
+
+//var totalWins = 0 {
+//    didSet {
+//        newRound()
+//    }
+//}
+//var totalLosses = 0 {
+//    didSet {
+//        newRound()
+//    }
+//}
+
+class ViewController: UIViewController {
+
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+    var currentGame: Game!
+    
+//    var formattedWord: String {
+//        var guessedWord = ""
+//        for letter in word {
+//            if guessedLetters.contains(letter) {
+//               guessedWord += "\(letter)"
+//            } else {
+//                guessedWord += "_"
+//            }
+//        }
+//        return guessedWord
+//    }
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+            incorrectMovesRemaining: incorrectMovesAllowed,
+            guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        
+        correctWordLabel.text = currentGame.formattedWord
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+//    let cast = ["Vivien", "Marlon", "Kim", "Karl"]
+//    let list = cast.jointed(separator: "", ")
+//        print(list)
+                            
+                            
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        // explain more....
+        currentGame.playerGuessed(letter: letter)
+        //updateUI()
+        updateGameState()
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+
+}
+
